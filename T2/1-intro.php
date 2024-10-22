c53abb99c79e985d44d6e6ba6f179c8cf5aaf1b8
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-22 18:22:29 +0200

    completing unit 2

--- T2/1-intro.php
+++ T2/1-intro.php
@@ -9,9 +9,28 @@
     <?php
 // necesita los cierres de linea
 // al ejecutar un php, solo muestra el html
-print "<u><b>Hola que tal por print</u></b>";
-print "<br><br>";
-echo "<u><b>Hola que tal por echol</u></b>";
+print "<u>Se puede imprimir pantalla con <b>print</u></b>\n";
+echo "<br><u>o con <b>echo</u></b>\n";
+
+
+$variableNum=7;
+$variableBool=false;
+$variableDouble=1.57;
+$variableString="Hola esto es una cadena";
+$paraDump=[1,2,3,5,6,"lilio"];
+
+print "<br>Las variables van como en JS, sin tipar, podemos usar gettype para ver su tipo\n";
+
+print "<br> usando el gettype con una variable, su tipo es: ".gettype($variableNum);
+echo "<br> usando el gettype con otra variable, su tipo es: ".gettype($variableBool);
+
+echo "<br>podemos usar ( is_bool(), is_integer(), is_scalar(), is_array(), is_null(), is_string(), is_float(), is_numeric() ) <br>para ver si son de determinado tipo\n";
+
+echo "<br> usando el is_numeric con una variable integer, es numerico?: ".(is_numeric($variableNum)?'true':'false');
+echo "<br> usando el is_numeric con otra variable string, es numerico?: ".(is_numeric($variableString)?'true':'false');
+print "<br>";
+echo "<br> usando el var_dump() con otra variable array, mostrara tipo y valor: ";
+echo var_dump($paraDump);
 
 ?>
 </body>
---
+++ T2/exercises-T2/exercise-extra.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $contador1=0;
+        $contador2=0;
+        $contador3=0;
+        $contador4=0;
+        $contador5=0;
+        $contador6=0;
+
+        for ($i=0; $i <100 ; $i++) { 
+            $dado= rand(1,6);
+    
+                if($dado==1){
+                    $contador1++;
+                }elseif($dado==2){
+                    $contador2++;
+                }elseif($dado==3){
+                    $contador3++;
+                }elseif($dado==4){
+                    $contador4++;
+                }elseif($dado==5){
+                    $contador5++;
+                }else{
+                    $contador6++;
+                }
+    
+        }
+print "<img src='./img/1.png' width=60 height=60/> <h3> salió un total de $contador1 veces </h3>";
+print "<img src='./img/2.png' width=60 height=60/> <h3> salió un total de $contador2 veces </h3>";
+print "<img src='./img/3.png' width=60 height=60/> <h3> salió un total de $contador3 veces </h3>";
+print "<img src='./img/4.png' width=60 height=60/> <h3> salió un total de $contador4 veces </h3>";
+print "<img src='./img/5.png' width=60 height=60/> <h3> salió un total de $contador5 veces </h3>";
+print "<img src='./img/6.png' width=60 height=60/> <h3> salió un total de $contador6 veces </h3>";
+
+
+
+
+
+?>
+</body>
+</html>
