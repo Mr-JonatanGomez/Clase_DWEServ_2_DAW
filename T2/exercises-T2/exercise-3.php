02323f9e22350fa849647753b44a044a0f5b44e6
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-22 14:22:59 +0200

    resolving exercises

--- T2/exercises-T2/exercise-3.php
+++ T2/exercises-T2/exercise-3.php
@@ -7,6 +7,7 @@
     <style>
         table{
             width: 50%;
+            margin: 0 auto;
             border: solid 2px;
         }
         tr{
@@ -23,13 +24,13 @@
     <?php
         // Muestra la tabla de multiplicar de un número generado de manera aleatoria entre 1 y 10. El resultado en formato <table>
         $numeroRand = rand(1,10);
-        echo "El numero aleatorio esta vez fue: ". $numeroRand ."<br>";
+        /* echo "El numero aleatorio esta vez fue: ". $numeroRand ."<br>";
         for ($i=0; $i <=10 ; $i++) { 
             print $numeroRand ." x ". $i ." = ". $numeroRand*$i ."<br>";
-        }
-//todo hacer la tabla, esperando respuesta del profesor
+        } */
+
         print "<br>";
-        //echo "<tr>"
+       
 
         print "<table>";
         echo "<tr>";
