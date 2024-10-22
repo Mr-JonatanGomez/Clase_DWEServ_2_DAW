1ff9cdedb42cc48f526ea9bec3651c0fca5a5fb2
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-22 13:09:08 +0200

    resolving exercises

--- T2/exercises-T2/exercise-6.php
+++ T2/exercises-T2/exercise-6.php
@@ -42,25 +42,50 @@ for ($i = 0; $i < 3; $i++) {
     </style>
 </head>
 <body>
-    <h1>Números Aleatorios y su Cuadrado</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Número</th>
-                <th>Cuadrado del Número</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            // Mostrar los números y su cuadrado
-            foreach ($numeros as $numero) {
-                echo "<tr>";
-                echo "<td>$numero</td>";
-                echo "<td>" . ($numero * $numero) . "</td>";
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-    </table>
+    
+    
+    <?php
+    // Un programa que genere 2 tiradas de 3 dados(simulando 2 jugadores). 
+    //Muestre las dos tiradas y me diga cual tiene mayor puntuación(sumando las tiradas)
+    $puntosJ1=0;
+    $puntosJ2=0;
+    
+    print "<h1>Turno Jugador 1</h1>";
+
+    for ($i=0; $i <3 ; $i++) { 
+        $dado= rand(1,6);
+        print "<img src='./img/$dado.png' width=100 height=100/>";
+        $puntosJ1+= $dado;
+        
+    }
+    print "<br>";
+    print "<h3>El jugador 1 ha tenido $puntosJ1 puntos</h3>";
+
+    print "<hr>";
+
+    print "<h2>Turno Jugador 2</h2>";
+
+    for ($i=0; $i <3 ; $i++) { 
+        $dado= rand(1,6);
+        print "<img src='./img/$dado.png' width=100 height=100/>";
+        $puntosJ2+= $dado;
+        
+    }
+    print "<br>";
+    print "<h3>El jugador 2 ha tenido $puntosJ2 puntos</h3>";
+    
+    print "<hr>";
+    print "<hr>";
+
+    if ($puntosJ1 > $puntosJ2) {
+        print "<h2>El ganador ha sido jugador1 con una diferencia de ".$puntosJ1-$puntosJ2 ." puntos</h2>";
+    }elseif ($puntosJ1 < $puntosJ2) {
+        print "<h2>El ganador ha sido jugador2 con una diferencia de ".$puntosJ2-$puntosJ1 ." puntos</h2>";
+    }else{
+        print "<h2>La partida ha finalizado con empate</h2>";
+    }
+    
+    ?>    
+    
 </body>
 </html>
