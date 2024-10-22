f5b5e69d223df432a0824c67de620e083f9fa253
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-22 11:26:25 +0200

    resolving exercises

--- T2/exercises-T2/exercise-3.php
+++ T2/exercises-T2/exercise-3.php
@@ -7,7 +7,14 @@
 </head>
 <body>
     <?php
-
+        // Muestra la tabla de multiplicar de un número generado de manera aleatoria entre 1 y 10. El resultado en formato <table>
+        $numeroRand = rand(1,10);
+        echo "El numero aleatorio esta vez fue: ". $numeroRand ."<br>";
+        for ($i=0; $i <=10 ; $i++) { 
+            print $numeroRand ." x ". $i ." = ". $numeroRand*$i ."<br>";
+        }
+//todo hacer la tabla, esperando respuesta del profesor
     ?>
+
 </body>
 </html>
--- T2/exercises-T2/exercise-4.php
+++ T2/exercises-T2/exercise-4.php
@@ -7,6 +7,25 @@
 </head>
 <body>
     <?php
+        /*Realiza un programa que nos diga cuántos dígitos tiene un número 
+        aleatorio entre (0 y 9999). Mostrar el número y la cantidad de dígitos.*/
+
+        $numRand = rand(0,9999);
+        print "El numero aleatorio ha entre 0 y 9999 ha sido: " . $numRand ."<br>";
+        //v1
+        $numDigit = strlen($numRand);
+        print "El numero $numRand tiene una cantidad de $numDigit digitos, contados con strlen <br>";
+
+        //v2
+        if ($numRand <=10) { // no necesitamos corte al negativo, porque el rango es 0-9999
+            print "El numero $numRand tiene una cantidad de 1 digito, sacado con IF & ELSEIF";
+        }elseif ($numRand <=100) {
+            print "El numero $numRand tiene una cantidad de 2 digitos, sacado con IF & ELSEIF";
+        }elseif ($numRand <=1000) {
+            print "El numero $numRand tiene una cantidad de 3 digitos, sacado con IF & ELSEIF";
+        }elseif ($numRand <=10000) {
+            print "El numero $numRand tiene una cantidad de 4 digitos, sacado con IF & ELSEIF";
+        }
 
     ?>
 </body>
