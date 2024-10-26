03f3f5d68c4036d4e27eb7008a5597fc67b35ae1
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-26 19:20:28 +0200

    completing unit 2.2

---
+++ T2/3-control-structures.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<?php // && (and)  ||(OR)  !(NOT)
+//print igual que en java for, if, elseif, while
+
+$diaMes = rand(1,31);
+echo $diaMes."<br>";
+
+if ( $diaMes < 7 ){
+     echo 'Estamos a primeros de mes';
+}
+elseif ( $diaMes >= 7 && $diaMes <=23 ){
+    echo 'Es mediados de mes ( excepto los casos 15,16, que justo es la mitad';
+}elseif ( $diaMes == 15 || $diaMes == 16 ){
+    echo 'Justo estan en mitad, 15 o 16';
+}else { 
+    echo 'Es final de mes';
+}
+
+echo "Los bucles while, do while, no los voy a mostrart, es lo mismo que Java y JavaScript\n";
+
+
+?>
+
+
+</body>
+</html>
