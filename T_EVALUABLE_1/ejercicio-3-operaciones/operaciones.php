fd5081cb77fd0c7d71a927ce97951be2bfd52b64
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-11-06 23:48:14 +0100

    DWES evaluable

---
+++ T_EVALUABLE_1/ejercicio-3-operaciones/operaciones.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>web-form</title>
+        <link rel="stylesheet" href="./style.css" />
+    </head>
+    <body>
+        <h1>Operaciones matemáticas</h1>
+        <div class="container">
+            <?php
+                $numA=0;
+                $numB=0;
+            ?>
+            <div class="form">
+
+                <form>
+                    <label>Introduce un número</label>
+                    
+    
+    
+                </form>
+
+            </div>
+        </div>
+    </body>
+</html>
